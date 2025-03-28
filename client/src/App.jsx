992152f06a7da92f63d8e5d86b9add972dae318c
Atireg <134270279+Atireg@users.noreply.github.com>
2025-03-28 09:56:49 +0100

    add react router with details page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import { Routes, Route } from 'react-router'
+
 import Header from './components/header/Header'
 import Details from './components/details/Details'
 import Footer from './components/footer/Footer'
+import './App.css'
 
 function App() {
-
 	return (
 		<>
 			<Header />
-			<Details />
+
+			<Routes>
+				<Route path='/' element={<Details />}/>
+			</Routes>
+			{/* <Details /> */}
 			<Footer />
 		</>
 	)
