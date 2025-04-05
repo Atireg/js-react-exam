753909a87a8b88ded1a205cfd25c06776e02d3bc
Atireg <134270279+Atireg@users.noreply.github.com>
2025-04-05 09:32:46 +0200

    add user context consumer

--- client/src/components/elements-catalog/ElementsCatalog.jsx
+++ client/src/components/elements-catalog/ElementsCatalog.jsx
@@ -1,13 +1,19 @@
 import { Component } from 'react';
 import ElementsList from './ElementsList';
+import { UserContext } from '../../contexts/UserContext';
 
 export default class ElementsCatalog extends Component {
     render() {
         return (
-            <>
-                <h1>Elements Catalog</h1>
-               <ElementsList />
-            </>
+            <UserContext.Consumer>
+                {(context) => (
+                    <section>
+                        <h1>Elements Catalog</h1>
+                        <h2>Hello, {context.username}! Harvest your ReUse elements here!</h2>
+                        <ElementsList />
+                    </section>
+                )}
+            </UserContext.Consumer>
         )
     }
 }
