0b4668d6b2333176a380042d4ee414a1c42e178b
Atireg <134270279+Atireg@users.noreply.github.com>
2025-04-07 21:42:04 +0200

    adjust basket

--- client/src/components/elements-catalog/element-item/ElementItem.jsx
+++ client/src/components/elements-catalog/element-item/ElementItem.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
+import { useAddToBasket } from "../../../api/basketApi";
+import { useGetOneElement } from "../../../api/elementsApi";
 import { useGetOneProject } from "../../../api/projectsApi";
 import idSlicer from "../../../utils/idSlicer";
 
-export default function ElementItem (
+export default function ElementItem(
     {
         id,
         profileType,
         projectId,
         length,
+    
     }
 ) {
 
     const { project } = useGetOneProject(projectId);
+    const { element } = useGetOneElement(id);
+    const [ elementForBasket, setElementForBasket ] = useState({});
+    const { addToBasket } = useAddToBasket();
+
+
+    const getElementIdHandler = () => {
+        setElementForBasket(element);
+    };
+
+    if (Object.keys(elementForBasket).length !== 0) { //TODO Add a check if the element is already there
+        addToBasket(elementForBasket);
+    };
 
     return (
         <li className='elements-item'>
@@ -18,7 +34,7 @@ export default function ElementItem (
             <p>Profil: {profileType}</p>
             <p>Project: {project.name}</p>
             <p>Length: {length}m</p>
-            <button className="small-button">Grab!</button>
+            <button onClick={() => getElementIdHandler(id)} className="small-button">Grab!</button>
         </li>
     )
 }
